d2ca46c3508a31424f3d519008d70e10e952de4d
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-07-21 15:04:28 +0700

    chore(main): change menu header and input

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+from config.app import APPConfig
 from handlers.get_url import get_data_from_search_engine
 from handlers.file import generate_file_name, write_to_file
 
 def main():
-  # Clear the screen
   print("\033[H\033[J")
-
-  print("Python Web Information Gathering")
+  print(f"Welcome to {APPConfig['name']} v{APPConfig['version']}")
+  print(f"{APPConfig['description']}")
+  print(f"Developed by {APPConfig['author']} ({APPConfig['github']})")
   print("=====================================")
-  domain = input("Enter domain: ")
-  keyword = input("Enter keyword: ")
+  domain = input("Enter domain (use * for wildcard): ")
+  keyword = input("Enter keyword (ex: best movie): ")
   print("=====================================")
   print("Searching for urls...")
   urls = get_data_from_search_engine(domain, keyword)
