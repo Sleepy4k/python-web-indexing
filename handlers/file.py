e2fa1a3ac874814a851a1d19b8882148c8c33243
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-07-22 19:47:49 +0700

    chore(handler): integrate file with unit test

--- handlers/file.py
+++ handlers/file.py
@@ -1,24 +1,30 @@
+import os
 import random
 import datetime
 from config.app import FILEPATHConfig
 
-def generate_file_name():
+def generate_file_name(number=None):
   now = datetime.datetime.now()
   date = now.strftime("%Y%m%d")
-  random_number = random.randint(1000, 9999)
+  random_number = number or random.randint(1000, 9999)
 
   return f"urls-{date}-{str(random_number)}.txt"
 
-def write_to_file(file_name, urls):
+def write_to_file(file_name, urls, path=None, test=False):
   if not urls or len(urls) == 0:
-    print("Data urls is empty, nothing to write to file, skipping process")
+    not test and print("Data urls is empty, nothing to write to file, skipping process")
     return
 
-  file = open(f"{FILEPATHConfig['output_path']}/{file_name}", "w")
+  path = path or FILEPATHConfig['output_path']
+
+  not os.path.exists(path) and os.makedirs(path)
+  not os.path.exists(f"{path}/{file_name}") and open(f"{path}/{file_name}", "w").close()
+
+  file = open(f"{path}/{file_name}", "w")
 
   for url in urls:
     file.write(url + "\n")
 
   file.close()
 
-  print(f"File {file_name} has been created with {len(urls)} urls")
+  not test and print(f"File {file_name} has been created with {len(urls)} urls")
