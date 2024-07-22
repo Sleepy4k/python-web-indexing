5802aa26b3559dbc2c0f0655e16283b1815ad6ef
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-07-22 19:48:41 +0700

    chore(util): add function get user agent from config

--- utils/generate_agent.py
+++ utils/generate_agent.py
@@ -1,6 +1,27 @@
 import random
 import utils.debug as debug
-from config.user_agent import get_user_agent
+from config.app import FILEPATHConfig
+from config.user_agent import USERAGENT
+
+def get_user_agent(path=None):
+  path = path or FILEPATHConfig['user_agent']
+
+  if not path:
+    return USERAGENT
+
+  try:
+    file = open(path, 'r')
+  except FileNotFoundError:
+    return USERAGENT
+
+  user_agent = file.readlines()
+
+  if not user_agent or len(user_agent) == 0:
+    return USERAGENT
+
+  file.close()
+
+  return USERAGENT + user_agent
 
 def generate_user_agent():
   list_agent = get_user_agent()
