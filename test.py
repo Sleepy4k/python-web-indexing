39b50173bb95c9848dc3893db519d8bffbadd80f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-07-22 19:49:06 +0700

    chore(test): add new unit test and clean up it

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
 import unittest
-from tests.check_url_test import TestCheckURL
-from tests.generate_agent_test import TestGenerateAgent
+from tests.utils.generate_agent_test import TestGenerateAgent
+from tests.handlers.file_test import TestGenerateFileName, TestWriteToFile
+from tests.utils.check_url_test import TestCheckDuplicateURL, TestIsURLValid
+from tests.handlers.get_url_test import TestGenerateQuery, TestBypassSafeSearch
 
 def main():
   loader = unittest.TestLoader()
   test_case = [
-    loader.loadTestsFromTestCase(TestCheckURL),
-    loader.loadTestsFromTestCase(TestGenerateAgent)
+    # Load Check URL Test
+    loader.loadTestsFromTestCase(TestIsURLValid),
+    loader.loadTestsFromTestCase(TestCheckDuplicateURL),
+
+    # Load Agent Test
+    loader.loadTestsFromTestCase(TestGenerateAgent),
+
+    # Load File Test
+    loader.loadTestsFromTestCase(TestWriteToFile),
+    loader.loadTestsFromTestCase(TestGenerateFileName),
+
+    # Load Get URL Test
+    loader.loadTestsFromTestCase(TestGenerateQuery),
+    loader.loadTestsFromTestCase(TestBypassSafeSearch),
   ]
 
   suite = unittest.TestSuite(test_case)
