7c5dad9fcd849d612256e7342843903551fe361d
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-07-25 00:14:13 +0700

    chore(util): make generated user agent will not duplicated

--- utils/generate_agent.py
+++ utils/generate_agent.py
@@ -3,6 +3,8 @@ import utils.debug as debug
 from config.app import FILEPATHConfig
 from config.user_agent import USERAGENT
 
+used_user_agent = []
+
 def get_user_agent(path=None):
   path = path or FILEPATHConfig['user_agent']
 
@@ -24,9 +26,15 @@ def get_user_agent(path=None):
   return USERAGENT + user_agent
 
 def generate_user_agent():
+  if len(used_user_agent) == len(USERAGENT):
+    used_user_agent.clear()
+
   list_agent = get_user_agent()
   length = len(list_agent) - 1
   user_agent = list_agent[random.randint(0, length)]
   debug.log("User agent generated: " + user_agent)
+
+  if user_agent in used_user_agent:
+    return generate_user_agent()
 
   return user_agent
